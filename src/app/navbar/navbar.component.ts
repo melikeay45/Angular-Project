f2351164e2d8fe8ffa8c7b53b02d077603db64c0
Melike Aydın <melike.ay45@gmail.com>
2024-01-22 03:00:34 +0300

    Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+//Api den http isteği ile veri çekmek için ekledim
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
