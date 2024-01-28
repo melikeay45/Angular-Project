5c91969cca1ac2eabc00f0dc3a458b89c3af41ae
Melike Aydın <melike.ay45@gmail.com>
2024-01-29 01:30:22 +0300

    register SignUp

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { ApiResult } from '../../Shared/models/system.model';
 import { environment } from '../../environments/environment';
 import { User } from '../../Shared/models/user.model';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,12 @@ import { User } from '../../Shared/models/user.model';
 export class UserService {
   readonly APIAddress = environment.APIAddress + 'UserApi/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {}
 
   getUserByUserID(): Observable<ApiResult<string>> {
     return this.http.get<ApiResult<string>>(this.APIAddress + 'Get');
+  }
+  AddUser(user: any): Observable<any> {
+    return this.apiService.postTypeRequest('UserApi/Add', user);
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../Services/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  user: any = {
+    name: '',
+    surname: '',
+    username: '',
+    password: '',
+    email: '',
+    address: '',
+    phoneNumber: 0,
+    isDelete: false,
+    userType: true,
+  };
+
+  constructor(private userService: UserService) {}
+
+  SignUp(): void {
+    this.userService.AddUser(this.user).subscribe(
+      (response) => {
+        console.log(this.user);
+        console.log('İstek başarılı:', response);
+      },
+      (error) => {
+        console.error('İstek hatası:', error);
+      }
+    );
+  }
+}
