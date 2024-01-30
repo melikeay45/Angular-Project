0fb6a8bb9c02cedd17552b2fb03f9758afd822a6
Melike Aydın <melike.ay45@gmail.com>
2024-01-30 22:27:28 +0300

    add footer and edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/NavbarComponent';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FooterComponent } from './footer/footer.component';
 
 //Api istekleri için
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MatSnackBarModule],
+  imports: [RouterOutlet, NavbarComponent, MatSnackBarModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  mail = 'melike.ay45@gmail.com';
+  login: boolean = localStorage.getItem("token") !== null;
+  type: string = localStorage.getItem("type") !== null ? localStorage.getItem("type")! : "";  
 
 }
